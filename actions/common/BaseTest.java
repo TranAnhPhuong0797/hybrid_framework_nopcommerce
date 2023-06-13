80fc8a49c4b773da229a4a64728ff95d574e6f2b
TranAnhPhuong0797 <67380535+TranAnhPhuong0797@users.noreply.github.com>
2023-06-13 10:02:00 +0700

    Commit - setup run test on Multiple Browsers

--- actions/common/BaseTest.java
+++ actions/common/BaseTest.java
@@ -1,5 +1,33 @@
 package common;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	private WebDriver driver;
+	private String projectpath = System.getProperty("user.dir");
+	
+	protected WebDriver getBrowserName(String browserName) {
+		
+		if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectpath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectpath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if (browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectpath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		}else {
+			throw new RuntimeException("Browser name invalid");
+		}
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://demo.nopcommerce.com/");
+		
+		return driver;
+	}
 }
