1eeefb25096314db98769a445d4d732efde6d1d6
TranAnhPhuong0797 <67380535+TranAnhPhuong0797@users.noreply.github.com>
2023-06-14 11:23:56 +0700

    Commit - Set up - WebDriverManagement

--- actions/common/BaseTest.java
+++ actions/common/BaseTest.java
@@ -7,27 +7,59 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
-	private WebDriver driver;
-	private String projectpath = System.getProperty("user.dir");
+	private WebDriver driverBaseTest;
+	//private String projectpath = System.getProperty("user.dir");
 	
 	protected WebDriver getBrowserName(String browserName) {
 		
 		if(browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", projectpath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
+			//System.setProperty("webdriver.gecko.driver", projectpath + "\\browserDrivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
+			driverBaseTest = new FirefoxDriver();
 		}else if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectpath + "\\browserDrivers\\chromedriver.exe");
-			driver = new ChromeDriver();
+			//System.setProperty("webdriver.chrome.driver", projectpath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
+			driverBaseTest = new ChromeDriver();
 		}else if (browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", projectpath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
+			//System.setProperty("webdriver.edge.driver", projectpath + "\\browserDrivers\\msedgedriver.exe");
+			WebDriverManager.edgedriver().setup();
+			driverBaseTest = new EdgeDriver();
 		}else {
 			throw new RuntimeException("Browser name invalid");
 		}
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://demo.nopcommerce.com/");
+		driverBaseTest.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driverBaseTest.get("https://demo.nopcommerce.com/");
 		
-		return driver;
+		return driverBaseTest;
 	}
+	
+	// Head less
+//	protected WebDriver getBrowserName(String browserName) {
+//		
+//		if(browserName.equals("firefox")) {
+//			System.setProperty("webdriver.gecko.driver", projectpath + "\\browserDrivers\\geckodriver.exe");
+//			FirefoxOptions options = new FirefoxOptions();
+//			options.addArguments("--headless");
+//			options.addArguments("window-size=1920*1080");
+//			driver = new FirefoxDriver(options);
+//		}else if (browserName.equals("chrome")) {
+//			System.setProperty("webdriver.chrome.driver", projectpath + "\\browserDrivers\\chromedriver.exe");
+//			ChromeOptions options = new ChromeOptions();
+//			options.addArguments("--headless");
+//			options.addArguments("window-size=1920*1080");
+//			driver = new ChromeDriver(options);
+//		}else if (browserName.equals("edge")) {
+//			System.setProperty("webdriver.edge.driver", projectpath + "\\browserDrivers\\msedgedriver.exe");
+//			driver = new EdgeDriver();
+//		}else {
+//			throw new RuntimeException("Browser name invalid");
+//		}
+//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//		driver.get("https://demo.nopcommerce.com/");
+//		
+//		return driver;
+//	}
 }
